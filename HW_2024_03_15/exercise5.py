aab38ae4454629e45d58acc5b001b1040784c0d4
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-05-19 17:31:30 +0400

    HW_2024_03_15 проверена

--- HW_2024_03_15/exercise5.py
+++ HW_2024_03_15/exercise5.py
@@ -22,3 +22,8 @@ def callculation_score(word): #
 
 word = input().replace('Ё', 'Е')
 print(callculation_score(word))
+
+# Комментарий преподавателя:
+# Для более компактного и читаемого кода можно использовать генераторные выражения
+# для создания словаря scores_letters и для подсчета очков за слово.
+# score = sum(key for letter in word for key, value in scores_letters.items() if letter in value)
